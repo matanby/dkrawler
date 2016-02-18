56f495391a101d8625e47257e8a84ffaeec66bfa
matanby <matan.ben.yosef@gmail.com>
2016-02-18 16:35:09 +0200

    Created a new package which includes core modules. Also, united all reports under the reports.py module.

---
+++ core/dionysus.py
@@ -0,0 +1,126 @@
+#! /usr/bin/env python
+
+import logging
+import time
+
+import dns
+import dns.exception
+import dns.resolver
+import eventlet
+from pymongo import MongoClient
+
+import conf
+import dal
+import scanners
+
+# The shared database client used
+DB_CLIENT = None
+
+
+def configure_logging():
+    """
+    Configuring the loggers used
+    """
+
+    formatter = logging.Formatter(conf.LOG_FORMAT)
+
+    fh = logging.FileHandler(conf.LOG_FILE_PATH)
+    fh.setLevel(conf.LOG_LEVEL_FILE_HANDLER)
+    fh.setFormatter(formatter)
+
+    sh = logging.StreamHandler()
+    sh.setLevel(conf.LOG_LEVEL_STDOUT)
+    sh.setFormatter(formatter)
+
+    logger = logging.root
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(sh)
+    logger.addHandler(fh)
+
+
+def exec_retry_scan(scan_func, dns_resolver, domain, client):
+    num_timeouts = 0
+    while True:
+        try:
+            scan_func(dns_resolver, domain, client)
+            return
+        except dns.exception.Timeout as e:
+            num_timeouts += 1
+            logging.debug('Got %d timeouts when querying %s' % (num_timeouts, domain))
+            if num_timeouts == conf.MAX_TIMEOUTS:
+                raise
+
+
+def scan_domain(domain):
+    # After forking, the DB_CLIENT might not be set
+    global DB_CLIENT
+    if DB_CLIENT is None:
+        DB_CLIENT = MongoClient(conf.DATABASE_SERVER, conf.DATABASE_PORT)
+
+    dns_resolver = dns.resolver.Resolver()
+    dns_resolver.use_edns(0, 0, 4096)
+    dns_resolver.timeout = conf.QUERY_TIMEOUT
+    dns_resolver.lifetime = conf.QUERY_TIMEOUT
+    bad_seed = False
+    for scan_func in scanners.scan_functions:
+        try:
+            exec_retry_scan(scan_func, dns_resolver, domain, DB_CLIENT)
+        except dns.resolver.NXDOMAIN as e:
+            logging.warning('Got NXDomain when trying to scan %s, marking bad seed' % domain)
+            bad_seed = True
+            break
+        except dns.resolver.YXDOMAIN as e:
+            logging.warning('Got YXDomain when trying to scan %s, marking bad seed' % domain)
+            bad_seed = True
+            break
+        except dns.resolver.NoNameservers as e:
+            logging.warning('No NS for %s, marking bad seed' % domain)
+            bad_seed = True
+            break
+        except dns.resolver.NoAnswer as e:
+            logging.warning('No answer received for %s, skipping' % domain)
+        except dns.exception.Timeout as e:
+            logging.warning('Got timeout when querying %s, skipping' % domain)
+        except Exception, ex:
+            logging.exception('Unexpected exception when querying %s' % domain)
+
+    dal.update_seed(DB_CLIENT, domain, bad_seed)
+
+
+def main():
+    configure_logging()
+    client = MongoClient(conf.DATABASE_SERVER, conf.DATABASE_PORT)
+
+    # Monkey patch system libraries to behave be non-blocking.
+    eventlet.monkey_patch()
+
+    # Creating greenlets pool to which the scans are distributed
+    pool = eventlet.GreenPool(conf.GREENLETS_POOL_SIZE)
+
+    # Adding a scan task for each seed
+    logging.info('Searching for seeds')
+
+    scan_start_time = time.time()
+
+    cursor = client.dionysus.seeds.find({
+        'last_scan': {'$lt': time.time() - conf.RESCAN_PERIOD},
+        'bad': False
+    })
+
+    logging.info('Found %d seeds' % cursor.count())
+    domains_to_scan = []
+    for seed in cursor:
+        domain = str(seed['domain'])
+        if not client.dionysus.dnskey.find_one({'domain': domain}):
+            domains_to_scan.append(domain)
+            pool.spawn(scan_domain, domain)
+
+    # Waiting for the scan to end
+    pool.waitall()
+
+    scan_time = time.time() - scan_start_time
+    logging.info('Total scan time: %s seconds' % scan_time)
+
+
+if __name__ == "__main__":
+    main()
---
+++ core/reports.py
@@ -0,0 +1,84 @@
+from pymongo import MongoClient
+import os
+import itertools
+
+import conf
+
+
+def key_lengths():
+    client = MongoClient(conf.DATABASE_SERVER, conf.DATABASE_PORT)
+    cursor = client.dionysus.dnskey.find()
+
+    # Creating a key length histogram
+    n_lengths = {}
+    for entry in cursor:
+        if 'N' not in entry:
+            continue
+        n_len = len(entry['N'])
+        if n_len not in n_lengths:
+            n_lengths[n_len] = 1
+        else:
+            n_lengths[n_len] += 1
+
+    # Printing the actual key length histogram, sorted
+    for length in sorted(n_lengths.keys()):
+        print "%d bits : %d" % (length * 4, n_lengths[length])
+
+
+def create_moduli_file(moduli_file_path):
+    client = MongoClient(conf.DATABASE_SERVER, conf.DATABASE_PORT)
+    moduli_file = open(moduli_file_path, 'w')
+    cursor = client.dionysus.dnskey.find({'N': {'$exists': True}})
+    for entry in cursor:
+        moduli_file.write(entry['N'])
+        moduli_file.write('\n')
+    moduli_file.close()
+
+    # After the file is created, filter it through sort-uniq
+    os.system('cat %s | sort | uniq > %s_' % (moduli_file_path, moduli_file_path))
+    os.system('rm %s' % moduli_file_path)
+    os.system('mv %s_ %s' % (moduli_file_path, moduli_file_path))
+
+
+def find_duplicate_moduli():
+    client = MongoClient(conf.DATABASE_SERVER, conf.DATABASE_PORT)
+    cursor = client.dionysus.dnskey.find({'N': {'$exists': True}})
+    n_map = {}
+
+    for entry in cursor:
+        n = entry['N']
+        if n not in n_map:
+            n_map[n] = 1
+        else:
+            n_map[n] += 1
+
+    for n in n_map:
+        if n_map[n] > 1:
+            print "---------------------------------------------"
+            print "Duplicate modulus!"
+            print n
+            print "Found in:"
+            cursor = client.dionysus.dnskey.find({'N': n})
+            for entry in cursor:
+                print entry['domain']
+
+
+def find_vulnerable_moduli_info(vuln_moduli_file_path, gcd_file_path):
+    client = MongoClient(conf.DATABASE_SERVER, conf.DATABASE_PORT)
+    vuln_moduli_file = open(vuln_moduli_file_path, 'r')
+    gcd_file = open(gcd_file_path, 'r')
+
+    for N_line, gcd_line in itertools.izip(vuln_moduli_file, gcd_file):
+        N = N_line.strip()
+        gcd = gcd_line.strip()
+        cursor = client.dionysus.dnskey.find({'N': N})
+
+        print "---------------------------------------------"
+        print "N: %s" % N
+        print "gcd: %s" % gcd
+        for entry in cursor:
+            print "Domain: %s" % entry['domain']
+
+    vuln_moduli_file.close()
+    gcd_file.close()
+
