58c1aac66e7ef4609ae5899f1dbe05fb496b6d9e
matanby <matan.ben.yosef@gmail.com>
2016-03-02 16:22:19 +0200

    Minor visual adjustments.

--- conf.py
+++ conf.py
@@ -45,6 +45,7 @@ LOG_LEVEL_ERROR_FILE_HANDLER = logging.ERROR
 # The info log file used
 INFO_LOG_FILE_PATH = 'dionysus.log'
 
+
 # The error log file used
 ERROR_LOG_FILE_PATH = 'dionysus.err'
 
@@ -56,18 +57,23 @@ LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 # The timeout, in seconds, for each query performed
 QUERY_TIMEOUT = 7 
 
+
 # The upper limit of the number of results that should
 # be returned by the web app in a single query
 WEB_RESULTS_LIMIT = 20
 
+
 # The directory of the fastgcd binary
 FASTGCD_DIR = '/data/fastgcd/fastgcd/'
+
 
 # The path of the input file for the fastgcd binary
 FASTGCD_INPUT_FILE_PATH = FASTGCD_DIR + 'input_moduli.txt'
 
+
 # The path of the output file for the fastgcd binary (containing the vulnerable moduli)
 FASTGCD_OUTPUT_FILE_PATH = FASTGCD_DIR + 'vulnerable_moduli'
+
 
 # The path of the output GCDs after computing the fastgcd
 FASTGCD_GCD_FILE_PATH = FASTGCD_DIR + 'gcds'
